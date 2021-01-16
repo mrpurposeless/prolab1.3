47e1b2824a052d97ef2161e957d3fadb38457f69
Tahir Can Yıldız <73746947+mrpurposeless@users.noreply.github.com>
2021-01-16 22:22:47 +0300

    Add files via upload

---
+++ prolab3/cmake-build-debug/CMakeFiles/prolab3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prolab3.dir/main.c.obj"
+  "libprolab3.dll.a"
+  "prolab3.exe"
+  "prolab3.exe.manifest"
+  "prolab3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/prolab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
